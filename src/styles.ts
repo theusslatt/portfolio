6ec15d5fd6d25180757d45e6ba899cdfacf48e7d
Matheus Silveira <theunewba@gmail.com>
2025-03-15 23:00:50 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/styles.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+import App from './App'
+
+const EstiloGlobal = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+  }
+`
+export default EstiloGlobal
