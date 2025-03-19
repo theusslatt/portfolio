9813c5114b12f894a8dbd6d36da2d3d1b20386a9
Matheus Silveira <theunewba@gmail.com>
2025-03-19 18:53:36 -0300

    criando e estilizando tema dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+
 import Projetos from './containers/Projetos'
 import Sidebar from './containers/Sidebar'
 import Sobre from './containers/Sobre'
 import EstiloGlobal, { Container } from './styles'
+import TemaLight from './themes/light'
+import TemaDark from './themes/dark'
 
 function App() {
+  const [estaUsandoTemaDark, setEstaUsandoTemaDark] = useState(false)
+
+  function trocaTema() {
+    setEstaUsandoTemaDark(!estaUsandoTemaDark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={estaUsandoTemaDark ? TemaDark : TemaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
